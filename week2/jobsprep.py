d655e5dab8f302a1d97b5c10d437369cf3ec2010
ernie <s212571@dtu.dk>
2022-01-12 22:50:45 +0100

    jacobi experiment setup files

---
+++ week2/jobsprep.py
@@ -0,0 +1,76 @@
+#!/bin/python3
+
+num_threads = [1,2,4,8,16,24]
+prob_sizes = [10, 30, 50, 70, 90, 150, 200, 250, 300]
+versions = ['poisson_j_omp_v0','poisson_j_omp_v1','poisson_j_omp_v2']
+
+psv = 'PROBSIZE' 
+tnv = 'NTHREADS'
+exv = 'EXEVAR'
+
+# serial
+with open('omp_jacobi_simple.sub', 'r') as file :
+  filedata = file.read()
+  exe_lines = ''
+  for ps in prob_sizes:
+    # Replace the target string
+    filedata = filedata.replace(psv, str(ps))
+    filedata = filedata.replace(tnv, str(1))
+    filedata = filedata.replace(exv, 'jacobi_serial')
+
+    exe_lines += f'./$EXECUTABLE {ps} $EXECOPTS\n'
+
+  file_name_root = f'jacobi_serial'
+  filedata = filedata.replace('OUTFILENAME', file_name_root + '.out')
+  filedata = filedata.replace('ERRFILENAME', file_name_root + '.err')
+  filedata = filedata.replace('./$EXECUTABLE $EXECOPTS', exe_lines)
+  # Write the file out again
+  with open(f'experiments/jacobi_serial/{file_name_root}.sub', 'w') as file:
+    file.write(filedata)
+
+# middle versions
+for v in versions:
+  for nth in num_threads:
+    with open('omp_jacobi_simple.sub', 'r') as file:
+      filedata = file.read()
+      exe_lines = ''
+      for ps in prob_sizes:
+        # Replace the target string
+        filedata = filedata.replace(psv, str(ps))
+        filedata = filedata.replace(tnv, str(nth))
+        filedata = filedata.replace(exv, v)
+
+        exe_lines += f'./$EXECUTABLE {ps} $EXECOPTS\n'
+
+      file_name_root = f'{v}_{nth}'
+      filedata = filedata.replace('OUTFILENAME', file_name_root + '.out')
+      filedata = filedata.replace('ERRFILENAME', file_name_root + '.err')
+      filedata = filedata.replace('./$EXECUTABLE $EXECOPTS', exe_lines)
+      # Write the file out again
+      with open(f'experiments/{v}/{file_name_root}.sub', 'w') as file:
+        file.write(filedata)
+
+# dynamic scheduling
+schedules = ['static','dynamic,1','dynamic,2','dynamic,4','dynamic,8','dynamic,16','dynamic,20','dynamic,25']
+v = 'poisson_j_omp_v3'
+for sch in schedules:
+  for nth in num_threads:
+    with open('omp_jacobi_simple.sub', 'r') as file:
+      filedata = file.read()
+      exe_lines = ''
+      for ps in prob_sizes:
+        # Replace the target string
+        filedata = filedata.replace(psv, str(ps))
+        filedata = filedata.replace(tnv, str(nth))
+        filedata = filedata.replace(exv, v)
+        filedata = filedata.replace('export OMP_SCHEDULE=dynamic,50', f'export OMP_SCHEDULE={sch}')
+
+        exe_lines += f'./$EXECUTABLE {ps} $EXECOPTS\n'
+      
+      file_name_root = f'{v}_{nth}_{sch}'
+      filedata = filedata.replace('OUTFILENAME', file_name_root + '.out')
+      filedata = filedata.replace('ERRFILENAME', file_name_root + '.err')
+      filedata = filedata.replace('./$EXECUTABLE $EXECOPTS', exe_lines)
+      # Write the file out again
+      with open(f'experiments/{v}/{file_name_root}.sub', 'w') as file:
+        file.write(filedata)
