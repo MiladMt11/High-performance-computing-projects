305708fd5a467d787be3a945708cfa673c4da9c3
ernie <s212571@dtu.dk>
2022-01-19 14:43:11 +0100

    gpu1 version

--- week3/matmult/matmult.cu
+++ week3/matmult/matmult.cu
@@ -1,171 +1,140 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "cblas.h"
 #include <string.h>
+#include "cblas.h"
+#include <cuda_runtime.h>
+#include "cublas_v2.h"
+#include <helper_cuda.h>
 
 #define min(X, Y) ((X) < (Y) ? (X) : (Y))
 
 extern "C"
 {; // just for Ernie's IDE indentation config, please don't remove it :)
 
-// https://github.com/attractivechaos/matmul/blob/master/matmul.c
-double** mat_transpose(int n_rows, int n_cols, double* const* a)
+void matmult_nat(int m, int n, int k, double* A, double* B, double* C)
 {
-    int i, j;
-    double** m;
-    // m = dmalloc_2d(n_cols, n_rows);
-    if (m == NULL)
-    {
-        printf("Couldn't allocate new matrix!\n");
-        exit(1);
-    }
-    for (i = 0; i < n_rows; ++i)
-        for (j = 0; j < n_cols; ++j)
-            m[j][i] = a[i][j];
-    return m;
-}
-
-double** mat_transpose_blk(int n_rows, int n_cols, double* const* a)
-{
-    const int blk = 8;
-    int i1, i2, j;
-    double** m;
-    // m = dmalloc_2d(n_cols, n_rows);
-    if (m == NULL)
-    {
-        printf("Couldn't allocate new matrix!\n");
-        exit(1);
-    }
-    for (i1 = 0; i1 < n_rows; i1 += blk)
-        for (j = 0; j < n_cols; ++j)
-            for (i2 = 0; i2 < min(n_rows - i1, blk); i2++)
-                m[j][i1 + i2] = a[i1 + i2][j];
-    return m;
-}
-
-void matmult_nat(int m, int n, int k, double** A, double** B, double** C)
-{
+    // A(m,k) m - # of rows; k - # of columns
+    // B(k,n) k - # of rows; n - # of columns
+    // C(m,n) m - # of rows; n - # of columns
     int i, j, l;
-    memset(C[0], 0, sizeof(double) * m * n);
-    double** BT = mat_transpose_blk(k, n, B);
-    for (i = 0; i < m; i++)
+    memset(C, 0, sizeof(double) * m * n);
+    for (i = 0; i < m; i++) // i - row #
     {
-        for (j = 0; j < n; j++)
+        for (l = 0; l < k; l++) // k - row #
         {
-            for (l = 0; l < k; l++)
+            for (j = 0; j < n; j++) // j - col #
             {
-                C[i][j] += A[i][l] * BT[j][l];
+                C[i * n + j] += A[i * k + l] * B[l * n + j];
             }
         }
     }
-    // dfree_2d(BT);
 }
 
-void matmult_lib(int m, int n, int k, double** A, double** B, double** C)
+void matmult_lib(int m, int n, int k, double* A, double* B, double* C)
 {
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, 1, A[0], k, B[0], n, 0, C[0], n);
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, 1, A, k, B, n, 0, C, n);
 }
 
-void matmult_mkn(int m, int n, int k, double** A, double** B, double** C)
+void matmult_mkn(int m, int n, int k, double* A, double* B, double* C)
 {
     int i, j, l;
-    memset(C[0], 0, sizeof(double) * m * n);
+    memset(C, 0, sizeof(double) * m * n);
     for (i = 0; i < m; i++)
     {
         for (l = 0; l < k; l++)
         {
             for (j = 0; j < n; j++)
             {
-                C[i][j] += A[i][l] * B[l][j];
+                C[i * n + j] += A[i * k + l] * B[l * n + j];
             }
         }
     }
 }
 
-void matmult_mnk(int m, int n, int k, double** A, double** B, double** C)
+void matmult_mnk(int m, int n, int k, double* A, double* B, double* C)
 {
     int i, j, l;
-    memset(C[0], 0, sizeof(double) * m * n);
+    memset(C, 0, sizeof(double) * m * n);
     for (i = 0; i < m; i++)
     {
         for (j = 0; j < n; j++)
         {
             for (l = 0; l < k; l++)
             {
-                C[i][j] += A[i][l] * B[l][j];
+                C[i * n + j] += A[i * k + l] * B[l * n + j];
             }
         }
     }
 }
 
-void matmult_kmn(int m, int n, int k, double** A, double** B, double** C)
+void matmult_kmn(int m, int n, int k, double* A, double* B, double* C)
 {
     int i, j, l;
-    memset(C[0], 0, sizeof(double) * m * n);
+    memset(C, 0, sizeof(double) * m * n);
     for (l = 0; l < k; l++)
     {
         for (i = 0; i < m; i++)
         {
             for (j = 0; j < n; j++)
             {
-                C[i][j] += A[i][l] * B[l][j];
+                C[i * n + j] += A[i * k + l] * B[l * n + j];
             }
         }
     }
 }
 
-void matmult_knm(int m, int n, int k, double** A, double** B, double** C)
+void matmult_knm(int m, int n, int k, double* A, double* B, double* C)
 {
     int i, j, l;
-    memset(C[0], 0, sizeof(double) * m * n);
+    memset(C, 0, sizeof(double) * m * n);
     for (l = 0; l < k; l++)
     {
         for (j = 0; j < n; j++)
         {
             for (i = 0; i < m; i++)
             {
-                C[i][j] += A[i][l] * B[l][j];
+                C[i * n + j] += A[i * k + l] * B[l * n + j];
             }
         }
     }
 }
 
-void matmult_nmk(int m, int n, int k, double** A, double** B, double** C)
+void matmult_nmk(int m, int n, int k, double* A, double* B, double* C)
 {
     int i, j, l;
-    memset(C[0], 0, sizeof(double) * m * n);
+    memset(C, 0, sizeof(double) * m * n);
     for (j = 0; j < n; j++)
     {
         for (i = 0; i < m; i++)
         {
             for (l = 0; l < k; l++)
             {
-                C[i][j] += A[i][l] * B[l][j];
+                C[i * n + j] += A[i * k + l] * B[l * n + j];
             }
         }
     }
 }
 
-void matmult_nkm(int m, int n, int k, double** A, double** B, double** C)
+void matmult_nkm(int m, int n, int k, double* A, double* B, double* C)
 {
     int i, j, l;
-    memset(C[0], 0, sizeof(double) * m * n);
+    memset(C, 0, sizeof(double) * m * n);
     for (j = 0; j < n; j++)
     {
         for (l = 0; l < k; l++)
         {
             for (i = 0; i < m; i++)
             {
-                C[i][j] += A[i][l] * B[l][j];
+                C[i * n + j] += A[i * k + l] * B[l * n + j];
             }
         }
     }
 }
 
-void matmult_blk(int m, int n, int k, double** A, double** B, double** C, int bs)
+void matmult_blk(int m, int n, int k, double* A, double* B, double* C, int bs)
 {
     int i_block, j_block, l_block;
-    memset(C[0], 0, sizeof(double) * m * n);
+    memset(C, 0, sizeof(double) * m * n);
     for (i_block = 0; i_block < m; i_block += bs)
     {
         for (l_block = 0; l_block < k; l_block += bs)
@@ -182,7 +151,7 @@ void matmult_blk(int m, int n, int k, double** A, double** B, double** C, int bs
                     {
                         for (j = j_block; j < j_max; j++)
                         {
-                            C[i][j] += A[i][l] * B[l][j];
+                            C[i * n + j] += A[i * k + l] * B[l * n + j];
                         }
                     }
                 }
@@ -191,21 +160,58 @@ void matmult_blk(int m, int n, int k, double** A, double** B, double** C, int bs
     }
 }
 
-void matmult_gpu1(int m, int n, int k, double** A, double** B, double** C)
+#define BLOCK_SIZE 64
+// int bs = BLOCK_SIZE;
+// int block_num_x = n / bs + 1;
+// int block_num_y = m / bs + 1;
+// dim3 dimGrid(block_num_x, block_num_y, 1);
+// dim3 dimBlock(bs, bs, 1);
+
+__global__ void gpu1_kernel(int m, int n, int k, double* A, double* B, double* C)
 {
+    // A(m,k) m - # of rows; k - # of columns
+    // B(k,n) k - # of rows; n - # of columns
+    // C(m,n) m - # of rows; n - # of columns
     int i, j, l;
-    memset(C[0], 0, sizeof(double) * m * n);
-    for (j = 0; j < n; j++)
+    for (i = 0; i < m; i++)
     {
         for (l = 0; l < k; l++)
         {
-            for (i = 0; i < m; i++)
+            for (j = 0; j < n; j++)
             {
-                C[i][j] += A[i][l] * B[l][j];
+                C[i * n + j] += A[i * k + l] * B[l * n + j];
             }
         }
     }
 }
 
+// The matrix sizes of A and B are m×k and k×n, respectively, so that C has size m×n
+void matmult_gpu1(int m, int n, int k, double* A_h, double* B_h, double* C_h)
+{
+    // Allocate A_d, B_d, C_d
+    double* A_d, * B_d, * C_d;
+    cudaMalloc((void**)&A_d, m * k * sizeof(double));
+    cudaMalloc((void**)&B_d, k * n * sizeof(double));
+    cudaMalloc((void**)&C_d, m * n * sizeof(double));
+
+    // Transfer data
+    cudaMemcpy(A_d, A_h, m * k * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(B_d, B_h, k * n * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemset(C_d, 0, m * n * sizeof(int));
+
+    // Launch kernel and synchronize
+    gpu1_kernel << <1, 1 >> > (m, n, k, A_d, B_d, C_d);
+    cudaDeviceSynchronize();
+
+    // Copy result back to host
+    cudaMemcpy(A_h, A_d, m * k * sizeof(double), cudaMemcpyDeviceToHost);
+    cudaMemcpy(B_h, B_d, k * n * sizeof(double), cudaMemcpyDeviceToHost);
+    cudaMemcpy(C_h, C_d, m * n * sizeof(double), cudaMemcpyDeviceToHost);
+
+    // Free A_d, B_d, C_d
+    cudaFree(A_d);
+    cudaFree(B_d);
+    cudaFree(C_d);
+}
 
 }
